c7aab2d57b0b0ee23870505ec1d61092e0ef9872
Nayo do X <vittorio.mendes@soulasalle.com.br>
2025-09-06 10:33:00 -0300

    Add files via upload

---
+++ Java/teste17.js
@@ -0,0 +1,34 @@
+   /*
+   //alert("Pedra Massiva!!!!!!!!!!!!") (Só um alerta que dá para botar quando inícia a página)
+    //document.writeln ("Quebre o rato com um quadrado.") (texto normal que dá para botar pelo Java)
+   */
+
+   console.log("EITAAAAAAAAAAA") //Ao inspecionar o site, temos uma parte com nome de "console" do lado dos elementos, pode ser um texto temporário (caso digitado pelo inspecionar) ou um que aparece lá (caso digitado aqui)
+
+    pedra = "massiva"
+    pedramassiva = "gorda"
+
+    a = 15
+    m = 31
+    adic = a+m
+    sub = a-m
+    mult = a*m
+    divi = a/m
+
+   // document.writeln("Pedra é massiva e pedramassiva é gorda") ou
+   document.writeln("Pedra é "+pedra+" e a Pedra massiva é "+pedramassiva)
+   document.writeln("<h1> Todas as pedras deveram ser "+pedra+"!</h1>")
+   document.writeln("A + M = "+(a+m))
+
+   document.writeln("<br>Adição "+adic)
+   document.writeln("<br>Subtração "+sub)
+   document.writeln("<br>Multiplicação "+mult)
+   document.writeln("<br>Divisão "+divi) 
+   //ou fazer assim:
+   document.writeln(
+      "<br>A + M = "+(a+m)
+      +"<br>Adição "+adic
+      +"<br>Subtração "+sub
+      +"<br>Multiplicação "+mult
+      +"<br>Divisão "+divi
+   )
