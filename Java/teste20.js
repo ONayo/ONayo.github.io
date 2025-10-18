1d9ee02a5bfa91130c2cc1d45a91f8141543c776
Nayo do X <vittorio.mendes@soulasalle.com.br>
2025-10-18 10:29:47 -0300

    Add files via upload

--- Java/teste20.js
+++ Java/teste20.js
@@ -1,4 +1,4 @@
-function mudarCoisas(){
+/* function mudarCoisas(){
 
     largura = Number(document.getElementById('largura').value)
 
@@ -13,7 +13,39 @@ function mudarCoisas(){
 function mudarCor(){
 
     cor = document.getElementById('cor').value
-
     document.getElementById('caixa').style.backgroundColor = cor
+
+} */
+
+function mudarCaixa(){
+
+    caixa = document.getElementById('caixa')
+
+// Esses comentários são só para separação de cada item.
+    cor = document.getElementById('cor').value
+    caixa.style.backgroundColor = cor
+
+//
+    caixa.style.width = largura+'px'
+    largura = Number(document.getElementById('largura').value)
+
+//
+    caixa.style.height = altura+'px'
+    altura = Number(document.getElementById('altura').value)
+
+//
+    caixa.innerText = texto
+    texto = document.getElementById('texto').value
+
+//
+    borderadius = Number(document.getElementById('borderadius').value)
+    caixa.style.borderRadius = borderadius+'%'
+
+//
+
+    grosso = Number(document.getElementById('grosso').value)
+    tipo = document.getElementById('tipo').value
+    cor2 = document.getElementById('cor2').value
+    caixa.style.border = grosso+'px '+tipo+''+cor2
 
 }
