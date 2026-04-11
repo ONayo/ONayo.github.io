8eb4afbe3299cf13f5847fd0b34de1abe28a66d9
Nayo do X <vittorio.mendes@soulasalle.com.br>
2026-04-11 10:31:50 -0300

    Add files via upload

--- php/7.php
+++ php/7.php
@@ -3,11 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> 7th php</title>
+    <link rel="stylesheet" href="4.css">
 </head>
 <body>
     
-    
+    <h1>Banco de Dados</h1>
+    <p>Um banco de dados é um local onde armazenamos informações de forma organizada. Exemplos:</p>
+    <ul>
+        <li>Lista de alunos</li>
+        <li>Produtos de uma loja</li>
+        <li>Usuários de um sistema</li>
+    </ul>
+
+    <h1>MySQL</h1>
+    <p>O MySQL é um sistema gerenciador de banco de dados. Ele permite:</p>
+    <ul>
+        <li>Criar bancos de dados</li>
+        <li>Criar tabelas</li>
+        <li>Inserir, editar e apagar dados</li>
+        <li>Fazer consultas</li>
+    </ul>
+
+    <hr>
+
+    <h2>Exemplo de estrutura de tabela</h2>
+    <b>Tabela de funcionários:</b>
+
+    <table border>
+        <tr>
+            <th>id</th>
+            <th>nome</th>
+            <th>idade</th>
+        </tr>
+        <tr>
+            <th>0</th>
+            <th>Pedro</th>
+            <th>20</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>Pedra</th>
+            <th>22</th>
+        </tr>
+    </table>
+
+    <img id="blah" src="Captura de tela 2026-04-11 101948.png" alt="">
 
 </body>
 </html>
