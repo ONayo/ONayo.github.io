4368bdac9e3580e2427d3a035264ea640446a343
Nayo do X <vittorio.mendes@soulasalle.com.br>
2026-04-25 10:32:08 -0300

    Add files via upload

--- php/8-front.php
+++ php/8-front.php
@@ -10,11 +10,37 @@
     <h2>Adicionar Funcionário</h2>
 
     <form action="9-back.php" method="post">
-        <input type="text" name="nome" max="64" min="3" placeholder="Nome"> 
-        <input type="number" name="idade" placeholder="Idade">
+        <input type="text" name="nome" max="64" min="3" placeholder="Nome"> <br>
+        <input type="number" name="idade" placeholder="Idade"> <br>
         <input type="text" name="cargo" max="64" min="3" placeholder="Cargo"> <br>
         <button type="submit">Add</button>
     </form>
+
+    <hr>
+
+    <?php 
+    
+    $conexao = new mysqli('localhost', 'root', '', 'simyes');
+    
+    $sql = "SELECT * FROM workers";
+    $resultado = $conexao->query($sql);
+
+    // var_dump($resultado);
+
+    echo('<table border>');
+    echo('<tr>');
+    echo('<th>ID</th><th>NOME</th><th>IDADE</th><th>CARGO</th>');
+    echo('</tr>');
+    while( $linha = $resultado->fetch_assoc() ){
+        echo('<tr>');
+        echo('<td>'.$linha['ID'].'</td>');
+        echo('<td>'.$linha['nome'].'</td>');
+        echo('<td>'.$linha['idade'].'</td>');
+        echo('<td>'.$linha['cargo'].'</td>');
+        echo('</tr>');
+    }
+    echo('</table');
+    ?>
 
 </body>
 </html>
