08ec3b77f7dd496f62d971617f653e953f9d5988
ONayo_OFC <vittorio.mendes@soulasalle.com.br>
2026-06-27 10:35:19 -0300

    Add files via upload

---
+++ 15_signup.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="">
+</head>
+<body>
+    
+    <form action="">
+        <span>name</span> 
+        <input type="text"> <br>
+        <span>email</span>
+        <input type="text"> <br>
+        <span>birthday</span> 
+        <input type="text"> <br>
+        <span>genre</span>
+        <select name="" id="">
+            <option>don't say</option>
+            <option>masculine</option>
+            <option>feminine</option>
+            <option>other</option>
+        </select> <br>
+        <span>profile picture</span>
+        <input type="file"> <br>
+        <span>password</span>
+        <input type="password"> <br>
+        <span>confirm password</span>
+        <input type="password">
+        <button>Join</button>
+    </form>
+
+</body>
+</html>
